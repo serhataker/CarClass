2c2869e6dfe88e866699760dbb8a120ce63a5204
SerhatAker <81346603+serhataker@users.noreply.github.com>
2024-08-02 23:04:37 +0300

    Add project files.

---
+++ Car.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Design;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarClass
+{
+    internal class Car
+    {
+        //We define the car's field
+        private string _name;
+        private int _model;
+        private string _color;
+        private int _doorNumber;
+
+        public Car(string name,int model,string color)// we send to 3 parametres for the constructor
+        {
+                
+
+            _name = name;
+            _model = model;
+            _color = color;
+            _doorNumber = Dournumber(_doorNumber);// we assign the door number if the user input any value
+          ;
+        }
+
+        // we define the properties for the car object 
+        public string Name { get { return _name; } set { _name = value; } }
+        public int Model { get { return _model; } set { _model = value; } }
+        public string Color { get { return _color; } set { _color = value; } }
+
+        public int DoorNumber { get { return _doorNumber; } } // we only define get method because we define method calculate the dour number for the car
+
+
+        //Car door set method
+          public int Dournumber (int _doorNumber) {
+
+            if (!(_doorNumber == 2 ||_doorNumber==4))
+            {
+
+                return -1;
+
+               
+            }
+            else { return _doorNumber; }
+        
+        }
+
+
+    }
+    }
---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using CarClass;
+using Microsoft.VisualBasic;
+using System;
+using System.Runtime.ConstrainedExecution;
+using System.Runtime.Intrinsics.X86;
+namespace Carsncapsul
+{
+    class Cars
+    {
+
+        public static void Main (string[] args )
+        {
+          Car Car1=new Car("Audi",2012,"Green"); //instance for the Car1
+            Console.WriteLine($"Car Name:{Car1.Name} Car Model:{Car1.Model} Car Color:{Car1.Color} Car Door Number:{Car1.Dournumber(1)}");//Information Car1 and we use ncapsulation car door
+            Car Car2 = new Car("Audi", 2018, "Red"); ////instance for the Car2
+            Console.WriteLine($"Car Name:{Car2.Name} Car Model:{Car2.Model} Car Color:{Car2.Color} Car Door Number:{Car2.Dournumber(2)}");//Information Car2 and we use ncapsulation car door
+            Car Car3 = new Car("Mercedes", 2024, "White");//instance for the Car3
+            Console.WriteLine($"Car Name:{Car3.Name} Car Model:{Car3.Model} Car Color:{Car3.Color} Car Door Number:{Car3.Dournumber(4)}"); //Information Car3 and we use ncapsulation car door
+            Car Car4 = new Car("BMW", 2012, "Yellow");//instance for the Car4
+            Console.WriteLine($"Car Name:{Car3.Name} Car Model:{Car3.Model} Car Color:{Car3.Color} Car Door Number:{Car3.DoorNumber}");// Information Car4 and we use constructor for door
+        }
+    }
+
+}
